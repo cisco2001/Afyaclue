1bbab02facb781d82f7764d70d233f06f90f163e
Mtwa Mgimwa <mtwa2001@gmail.com>
2024-02-27 22:27:49 +0300

    add everything

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(window).load(function(){
+    $('.preloader').fadeOut('slow');
+
+    initializeSite();
+
+    (function() {
+        setTimeout(function(){window.scrollTo(0,0);},0);
+    })();
+});
+
+function initializeSite() {
+    "use strict";
+
+    //OUTLINE DIMENSION AND CENTER
+    (function() {
+        function centerInit(){
+
+            var sphereContent = $('.sphere'),
+                sphereHeight = sphereContent.height(),
+                parentHeight = $(window).height(),
+                topMargin = (parentHeight - sphereHeight) / 2;
+
+            sphereContent.css({
+                "margin-top" : topMargin+"px"
+            });
+
+            var heroContent = $('.hero'),
+                heroHeight = heroContent.height(),
+                heroTopMargin = (parentHeight - heroHeight) / 2;
+
+            heroContent.css({
+                "margin-top" : heroTopMargin+"px"
+            });
+
+        }
+
+        $(document).ready(centerInit);
+        $(window).resize(centerInit);
+    })();
+
+    // Init effect 
+    $('#scene').parallax();
+
+};
+
+$('#countdown').countdown({
+    date: "Mar 10 2024", // Update to a future date
+    render: function(data) {
+        var el = $(this.el);
+        el.empty()
+            .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+            .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+            .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+            .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    }
+});
